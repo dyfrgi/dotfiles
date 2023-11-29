fdfb105b1203df4a7a0d563fc203ac5da605c9c9
Michael Leuchtenburg <michael@slashhome.org>
2023-11-29 16:02:43 -0500

    Initial flake config

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Home manager flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/release-23.11";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }: {
+
+    defaultPackage.x86_64-linux = home-manager.defaultPackage.x86_64-linux;
+
+    homeConfigurations = {
+      "mleuchtenburg" = home-manager.lib.homeManagerConfiguration {
+        pkgs = import nixpkgs { system = "x86_64-linux"; };
+        modules = [ ./home.nix ];
+      };
+    };
+  };
+}
---
+++ home.nix
@@ -0,0 +1,6 @@
+{pkgs, ...}: {
+  home.username = "mleuchtenburg";
+  home.homeDirectory = "/home/mleuchtenburg";
+  home.stateVersion = "23.11";
+  programs.home-manager.enable = true;
+}
