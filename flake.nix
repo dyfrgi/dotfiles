78c2d721e7c737f4b72275cca234dd4b6b997f0e
Michael Leuchtenburg <michael@slashhome.org>
2023-11-30 10:55:57 -0500

    Set home-manager version to 23.11 to match nixpkgs

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/release-23.11";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
