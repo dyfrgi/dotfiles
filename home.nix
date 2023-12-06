f57a0733d259356d10a60c788182be8fa89ff9dc
Michael Leuchtenburg <michael@slashhome.org>
2023-12-06 11:28:51 -0500

    Fix xdg-open and add nvim-treesitter grammars

--- home.nix
+++ home.nix
@@ -3,7 +3,17 @@
   home.homeDirectory = "/home/mleuchtenburg";
   home.stateVersion = "23.11";
   programs.home-manager.enable = true;
+  home.sessionVariables = {
+    NIXOS_XDG_OPEN_USE_PORTAL = 1;
+  };
+  programs.neovim = {
+      enable = true;
+      plugins = with pkgs.vimPlugins; [
+        nvim-treesitter.withAllGrammars
+      ];
+  };
   home.packages = [
-    pkgs.neovim
+    pkgs.logseq
+    pkgs.xdg-utils
   ];
 }
