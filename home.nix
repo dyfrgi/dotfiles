28b8226c64e0104e864d8b60eb6eeaf09bfb29aa
Michael Leuchtenburg <michael@slashhome.org>
2024-03-05 12:09:38 -0500

    Add perftools support to luajit for neovim
    Also adds a couple packages, lazygit and yt-dlp.

--- home.nix
+++ home.nix
@@ -1,11 +1,24 @@
-{pkgs, ...}: {
+{pkgs, ...}:
+let
+  withCompilerFlags = (package: flags: package.overrideAttrs(old: {
+      env = (old.env or {}) // { NIX_CFLAGS_COMPILE = toString (old.env.NIX_CFLAGS_COMPILE or "") + " ${toString flags}"; };
+  }));
+in
+{
   home.username = "mleuchtenburg";
   home.homeDirectory = "/home/mleuchtenburg";
   home.stateVersion = "23.11";
+  home.enableDebugInfo = true;
   programs.home-manager.enable = true;
   home.sessionVariables = {
     NIXOS_XDG_OPEN_USE_PORTAL = 1;
   };
+  nixpkgs.overlays = [
+    (final: prev: {
+# the call to override is needed because it sets up all the spliced versions of luajit
+      luajit = (withCompilerFlags prev.luajit [ "-DLUAJIT_USE_PERFTOOLS" ]).override { self = final.luajit; };
+    })
+  ];
   programs.neovim = {
       enable = true;
       plugins = with pkgs.vimPlugins; [
@@ -13,7 +26,9 @@
       ];
   };
   home.packages = [
+    pkgs.lazygit
     pkgs.logseq
     pkgs.xdg-utils
+    pkgs.yt-dlp
   ];
 }
