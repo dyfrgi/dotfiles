750bddc355f490a3da664b17d2338d186814aa60
Michael Leuchtenburg <michael@slashhome.org>
2024-03-06 12:35:42 -0500

    neovim: Remove defaults from nvim-cmp config, also don't autoconfirm

--- home/.config/nvim/lua/plugins/nvim-cmp.lua
+++ home/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -1,48 +1,12 @@
-opts = function()
-  vim.api.nvim_set_hl(0, "CmpGhostText", { link = "Comment", default = true })
-  local cmp = require("cmp")
-  local defaults = require("cmp.config.default")()
-  return {
+local cmp = require("cmp")
+return {
+ "hrsh7th/nvim-cmp",
+  opts = {
     completion = {
-      completeopt = "menu,menuone,noinsert",
+      completeopt = "menu,menuone,noselect",
     },
     mapping = cmp.mapping.preset.insert({
-      ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-      ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-      ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-      ["<C-f>"] = cmp.mapping.scroll_docs(4),
-      ["<C-Space>"] = cmp.mapping.complete(),
-      ["<C-e>"] = cmp.mapping.abort(),
-      ["<CR>"] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-      ["<S-CR>"] = cmp.mapping.confirm({
-        behavior = cmp.ConfirmBehavior.Replace,
-        select = true,
-      }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-      ["<C-CR>"] = function(fallback)
-        cmp.abort()
-        fallback()
-      end,
+      ["<CR>"] = cmp.mapping.confirm({ select = false }),
     }),
-    sources = cmp.config.sources({
-      { name = "nvim_lsp" },
-      { name = "path" },
-    }, {
-      { name = "buffer" },
-    }),
-    formatting = {
-      format = function(_, item)
-        local icons = require("lazyvim.config").icons.kinds
-        if icons[item.kind] then
-          item.kind = icons[item.kind] .. item.kind
-        end
-        return item
-      end,
-    },
-    experimental = {
-      ghost_text = {
-        hl_group = "CmpGhostText",
-      },
-    },
-    sorting = defaults.sorting,
   }
-end
+}
