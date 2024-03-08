23519696bb0e9bbaf54604ac30dbd3292e928dec
Michael Leuchtenburg <michael@slashhome.org>
2024-03-08 10:42:12 -0500

    Install configfiles using symlinks
    This is very anti-Nix, but duplicates my old dotfile setup. From here it
    will be possible to incrementally move configuration into Home Manager.
    
    Note that as of today, this requires using nix version 2.18, which can
    be installed with `sudo nix-env -i nix-2.18.1`. It may also be necessary
    to explicitly restart the nix-daemon service with `sudo systemctl
    daemon-reload` and `sudo systemctl restart nix-daemon.service`. This is
    needed due to NixOS/nix#9579, which unfortunately is also not fixed in
    nix 2.20.5, which is the new version in nixpkgs. It looks like
    NixOS/nix#9723 will fix this and will hopefully land before nix 2.21.

--- home.nix
+++ home.nix
@@ -1,8 +1,39 @@
-{pkgs, ...}:
+{pkgs, config, lib, ...}:
 let
+  inherit (builtins) foldl';
   withCompilerFlags = (package: flags: package.overrideAttrs(old: {
       env = (old.env or {}) // { NIX_CFLAGS_COMPILE = toString (old.env.NIX_CFLAGS_COMPILE or "") + " ${toString flags}"; };
   }));
+  dotfilesPath = "${config.home.homeDirectory}/.config/home-manager/";
+  linkHome = path: config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/home/${path}";
+  homeFilesToLink = [
+    ".dircolors"
+    ".etc/"
+    ".gitconfig"
+    ".gitignore_global"
+    ".gitk"
+    ".gtkrc-2.0"
+    ".inputrc"
+    ".mplayer/"
+    ".reportbugrc"
+    ".rtorrent.rc"
+    ".screenrc"
+    ".tmux/"
+    ".tmux.conf"
+    ".XCompose"
+    ".xmonad/"
+    ".xscreensaver"
+    ".xsession"
+    ".zsh/"
+    ".zshenv"
+    ".zshrc"
+  ];
+  xdgConfigFilesToLink = [
+    "awesome/"
+    "compton.conf"
+    "nvim/"
+    "taffybar/"
+  ];
 in
 {
   home.username = "mleuchtenburg";
@@ -34,4 +65,7 @@ in
     pkgs.xdg-utils
     pkgs.yt-dlp
   ];
+
+  home.file = foldl' (acc: elem: { "${elem}" = { source = linkHome elem; }; } // acc) {} homeFilesToLink;
+  xdg.configFile = foldl' (acc: elem: { "${elem}" = { source = linkHome ".config/${elem}"; }; } // acc) {} xdgConfigFilesToLink;
 }
