4eb18145aebadd4c26fcc77054b39018de95fd9a
Michael Leuchtenburg <michael@slashhome.org>
2024-05-27 16:01:58 -0400

    Switch to release 24.05 for nixos and home-manager

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "Home manager flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/release-24.05";
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
