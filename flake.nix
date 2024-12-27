0468d47b25a5fb30329aeb119db0d7546b93e8f0
Michael Leuchtenburg <michael@slashhome.org>
2024-12-27 16:44:49 -0500

    Upgrade to HM 24.11 and nixpkgs 24.11

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "Home manager flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/release-24.11";
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
