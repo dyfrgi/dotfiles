77b002e8216bbac06536e985452e84bef778b721
Michael Leuchtenburg <michael@slashhome.org>
2025-02-21 10:34:48 -0500

    switch devenv to unstable, format

--- home.nix
+++ home.nix
@@ -1,9 +1,20 @@
-{pkgs, config, lib, ...}:
+{
+  pkgs,
+  config,
+  pkgs-unstable,
+  lib,
+  ...
+}:
 let
   inherit (builtins) foldl';
-  withCompilerFlags = (package: flags: package.overrideAttrs(old: {
-      env = (old.env or {}) // { NIX_CFLAGS_COMPILE = toString (old.env.NIX_CFLAGS_COMPILE or "") + " ${toString flags}"; };
-  }));
+  withCompilerFlags = (
+    package: flags:
+    package.overrideAttrs (old: {
+      env = (old.env or { }) // {
+        NIX_CFLAGS_COMPILE = toString (old.env.NIX_CFLAGS_COMPILE or "") + " ${toString flags}";
+      };
+    })
+  );
   dotfilesPath = "${config.home.homeDirectory}/.config/home-manager/";
   linkHome = path: config.lib.file.mkOutOfStoreSymlink "${dotfilesPath}/home/${path}";
   homeFilesToLink = [
@@ -42,12 +53,12 @@ in
   imports = [ ./modules/nvim.nix ];
 
   xdg.enable = true; # set XDG_ env vars
-  xdg.systemDirs.data = ["${config.home.profileDirectory}/share"]; # add nix-profile to XDG_DATA_DIRS
+  xdg.systemDirs.data = [ "${config.home.profileDirectory}/share" ]; # add nix-profile to XDG_DATA_DIRS
 
   programs.home-manager.enable = true;
   programs.direnv.enable = true;
   programs.pyenv = {
-      enable = true;
+    enable = true;
   };
   programs.nix-index-database.comma.enable = true;
 
@@ -55,8 +66,8 @@ in
     acpi
     awscli2
     bat
-    devenv
-    fd                 # used by telescope-nvim
+    (pkgs-unstable.devenv)
+    fd # used by telescope-nvim
     fzf
     google-cloud-sdk
     lazygit
@@ -66,11 +77,32 @@ in
     yt-dlp
   ];
 
-  home.file = {
-    ".zsh" = { source = home/.zsh; recursive = true; };
-    ".zsh/rc/S15home-manager-extra".text = config.programs.zsh.initExtra;
-  } // foldl' (acc: elem: { "${elem}" = { source = linkHome elem; }; } // acc) {} homeFilesToLink;
-  xdg.configFile = foldl' (acc: elem: { "${elem}" = { source = linkHome ".config/${elem}"; }; } // acc) {} xdgConfigFilesToLink;
+  home.file =
+    {
+      ".zsh" = {
+        source = home/.zsh;
+        recursive = true;
+      };
+      ".zsh/rc/S15home-manager-extra".text = config.programs.zsh.initExtra;
+    }
+    // foldl' (
+      acc: elem:
+      {
+        "${elem}" = {
+          source = linkHome elem;
+        };
+      }
+      // acc
+    ) { } homeFilesToLink;
+  xdg.configFile = foldl' (
+    acc: elem:
+    {
+      "${elem}" = {
+        source = linkHome ".config/${elem}";
+      };
+    }
+    // acc
+  ) { } xdgConfigFilesToLink;
 
   services.ssh-agent.enable = true;
 }
