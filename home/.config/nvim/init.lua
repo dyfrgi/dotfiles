90a83d1d8281b7c6ad6d29f58b15e03a446ea77e
Michael Leuchtenburg <michael@slashhome.org>
2025-03-02 11:39:28 -0500

    nvim: disable forcing OSC 52 clipboard
    Necessary until https://github.com/neovim/neovim/issues/32699 is fixed.

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -1,18 +1,18 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- Clipboard configuration
-vim.g.clipboard = {
-  name = 'OSC 52',
-  copy = {
-    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
-    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
-  },
-  paste = {
-    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
-    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
-  },
-}
+-- -- Clipboard configuration
+-- vim.g.clipboard = {
+--   name = 'OSC 52',
+--   copy = {
+--     ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+--     ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+--   },
+--   paste = {
+--     ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+--     ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+--   },
+-- }
 vim.opt.clipboard = "unnamedplus" -- use the '+' register by default
 
 local opt = vim.opt
