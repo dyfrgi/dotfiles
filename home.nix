2e69c4fb058fc63a544b356a173b6595a9c3fca6
Michael Leuchtenburg <michael@slashhome.org>
2025-03-16 16:13:36 -0400

    ssh: switch from ssh-agent to gpg-agent

--- home.nix
+++ home.nix
@@ -98,5 +98,10 @@ in
     // acc
   ) { } xdgConfigFilesToLink;
 
-  services.ssh-agent.enable = true;
+  services.gpg-agent = {
+    enable = true;
+    enableSshSupport = true;
+    enableZshIntegration = true;
+    pinentryPackage = pkgs.pinentry-gtk2;
+  };
 }
