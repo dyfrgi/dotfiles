2c36dbf97704672a63f2433b05cc4c7184758bb5
Michael Leuchtenburg <michael@slashhome.org>
2025-04-13 11:31:01 -0400

    nvim: Add command to disable format on save

--- home/.config/nvim/lua/config/formatting.lua
+++ home/.config/nvim/lua/config/formatting.lua
@@ -3,8 +3,29 @@ require 'conform'.setup({
     lsp_format = "fallback"
   },
   notify_no_formatters = true,
-  format_on_save = {
-    timeout = 500
-  },
+  format_on_save = function(bufnr)
+    if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+      return
+    end
+    return { timeout_ms = 500, lsp_format = "fallback" }
+  end,
 })
 vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+
+vim.api.nvim_create_user_command("FormatDisable", function(args)
+  if args.bang then
+    -- FormatDisable! will disable formatting just for this buffer
+    vim.b.disable_autoformat = true
+  else
+    vim.g.disable_autoformat = true
+  end
+end, {
+  desc = "Disable autoformat-on-save",
+  bang = true,
+})
+vim.api.nvim_create_user_command("FormatEnable", function()
+  vim.b.disable_autoformat = false
+  vim.g.disable_autoformat = false
+end, {
+  desc = "Re-enable autoformat-on-save",
+})
