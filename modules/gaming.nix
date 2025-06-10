7cd18d017d495454a4ff2ca70c3ccf6643f3b88a
Michael Leuchtenburg <michael@slashhome.org>
2025-06-10 15:05:18 -0400

    gaming: add en-croissant and stockfish for chess

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -1,7 +1,9 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
+    en-croissant
     protonup
+    stockfish
   ];
   home.sessionVariables = {
     STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\\\${HOME}/.steam/root/compatibilitytools.d";
