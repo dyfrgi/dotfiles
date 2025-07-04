4a89a76673e3b40bd14f8b59e1de36e9f725da4a
Michael Leuchtenburg <michael@slashhome.org>
2025-07-04 12:19:43 -0400

    non-nixos: fix TERMINFO and systemd units
    Set TERMINFO_DIRS for ssh sessions (targets.genericLinux.enable sets it
    for systemd units) and TTY logins.
    
    Also, link the systemd units installed by home-manager into
    ~/.local/share/systemd/user/. This is a default searched path by systemd
    so with this, we do not need to set XDG_DATA_DIRS before running the
    user systemd.

--- modules-hm/non-nixos.nix
+++ modules-hm/non-nixos.nix
@@ -1,8 +1,34 @@
-{ pkgs, ... }:
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
 {
   home.packages = with pkgs; [ niri ];
 
   # generate .bashrc with environment variables to pick up e.g. XDG paths for login scripts
   programs.bash.enable = true;
   targets.genericLinux.enable = true;
+
+  # home-manager only sets TERMINFO_DIRS for systemd units, which does not include SSH logins
+  home.sessionVariables = {
+    TERMINFO_DIRS = "${config.home.homeDirectory}/.nix-profile/share/terminfo";
+  };
+
+  home.activation.linkSystemd =
+    let
+      inherit (lib) hm;
+    in
+    hm.dag.entryBefore [ "reloadSystemd" ] ''
+      mkdir -p $HOME/.local/share/systemd/user/
+      find $HOME/.local/share/systemd/user/ \
+        -type l \
+        -exec bash -c "readlink {} | grep -q $HOME/.nix-profile" \; \
+        -delete
+
+      find $HOME/.nix-profile/share/systemd/user \
+        -type f -o -type l \
+        -exec ln -s {} $HOME/.local/share/systemd/user/ \;
+    '';
 }
