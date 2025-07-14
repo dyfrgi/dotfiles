99181533e23d2f6571b023351cdb4c5f3001cc16
Michael Leuchtenburg <michael@slashhome.org>
2025-07-14 14:31:23 -0400

    nix: update nix-index-database module path
    Not sure why this didn't break until I got to a non-NixOS box - maybe
    due to old nix? When was builtins.warn added?

--- modules-hm/nix-index.nix
+++ modules-hm/nix-index.nix
@@ -1,6 +1,6 @@
 { inputs, ... }:
 {
-  imports = [ inputs.nix-index-database.hmModules.nix-index ];
+  imports = [ inputs.nix-index-database.homeModules.nix-index ];
 
   config = {
     programs = {
