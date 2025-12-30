6d0debc14e2a64197e229000767b1c110c6587bd
Michael Leuchtenburg <michael@slashhome.org>
2025-12-30 12:57:13 -0500

    gui: drop fuzzel overlay
    I shouldn't need this on 25.11 as it has the new pixman and uses the
    newer fuzzel.

--- overlays/default.nix
+++ overlays/default.nix
@@ -5,35 +5,9 @@
 {
   config = {
     nixpkgs.overlays = [
-      (self: super: {
-        iosvmata = super.callPackage ../packages/iosvmata.nix { };
-        niri-select-window-by-name = super.callPackage ../packages/niri-select-window-by-name { };
-
-        # TODO: Drop this when moving to nixpkgs 25.11
-        fuzzel = super.fuzzel.overrideAttrs (oldAttrs: {
-          version = "117dc1f6d134a7fe5c90de67fb7f695c139f354c";
-          src = super.fetchFromGitea {
-            domain = "codeberg.org";
-            owner = "dnkl";
-            repo = "fuzzel";
-            rev = "117dc1f6d134a7fe5c90de67fb7f695c139f354c";
-            hash = "sha256-QR5YAPgv0GjrNCfzz0xbFNLcoaJExeSQYI+9w1M13u8=";
-          };
-          buildInputs = (builtins.filter (pkg: pkg.name != "pixman") oldAttrs.buildInputs) ++ [
-            self.pixman-046
-          ];
-          nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ self.pixman-046 ];
-        });
-        pixman-046 = super.pixman.overrideAttrs (_: {
-          version = "0.46.4";
-          src = super.fetchurl {
-            urls = [
-              "mirror://xorg/individual/lib/pixman-0.46.4.tar.gz"
-              "https://cairographics.org/releases/pixman-0.46.4.tar.gz"
-            ];
-            hash = "sha256-0JxE68O9W+5wIcefki/o+y+1f3Mg9V6X/5kU0jRqWRw=";
-          };
-        });
+      (final: prev: {
+        iosvmata = prev.callPackage ../packages/iosvmata.nix { };
+        niri-select-window-by-name = prev.callPackage ../packages/niri-select-window-by-name { };
       })
     ];
   };
