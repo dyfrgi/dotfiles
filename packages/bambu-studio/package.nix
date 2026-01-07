ea934a8c2a1be43c9f47bf38e5455e6a318c5037
Michael Leuchtenburg <michael@slashhome.org>
2026-01-07 11:34:23 -0500

    bambu-studio: re-organize into separate file

---
+++ packages/bambu-studio/package.nix
@@ -0,0 +1,39 @@
+{
+  appimageTools,
+  fetchurl,
+  pkgs,
+}:
+let
+  name = "BambuStudio";
+  pname = "bambu-studio";
+  version = "02.04.00.70";
+  ubuntu_version = "24.04_PR-8834";
+  src = fetchurl {
+    url = "https://github.com/bambulab/BambuStudio/releases/download/v${version}/Bambu_Studio_ubuntu-${ubuntu_version}.AppImage";
+    sha256 = "sha256:26bc07dccb04df2e462b1e03a3766509201c46e27312a15844f6f5d7fdf1debd";
+  };
+in
+appimageTools.wrapType2 {
+  inherit
+    name
+    pname
+    version
+    src
+    ;
+
+  profile = ''
+    export SSL_CERT_FILE="${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
+    export GIO_MODULE_DIR="${pkgs.glib-networking}/lib/gio/modules/"
+  '';
+
+  extraPkgs =
+    pkgs: with pkgs; [
+      cacert
+      glib
+      glib-networking
+      gst_all_1.gst-plugins-bad
+      gst_all_1.gst-plugins-base
+      gst_all_1.gst-plugins-good
+      webkitgtk_4_1
+    ];
+}
