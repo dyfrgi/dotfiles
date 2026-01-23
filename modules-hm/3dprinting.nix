470ce35e1283465cb15fb3c12ec77b1de46e1b2a
Michael Leuchtenburg <michael@slashhome.org>
2026-01-22 22:05:10 -0500

    3dprinting: overlay for freecad to fix mesa crash

--- modules-hm/3dprinting.nix
+++ modules-hm/3dprinting.nix
@@ -1,9 +1,21 @@
 { pkgs, ... }:
+let
+  freecad-fixed = pkgs.symlinkJoin {
+    name = "freecad-wayland-fix";
+    paths = [ pkgs.freecad-wayland ];
+    buildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      wrapProgram $out/bin/FreeCAD \
+        --prefix MESA_LOADER_DRIVER_OVERRIDE : zink \
+        --prefix __EGL_VENDOR_LIBRARY_FILENAMES : ${pkgs.mesa}/share/glvnd/egl_vendor.d/50_mesa.json
+    '';
+  };
+in
 {
   home.packages = with pkgs; [
     bambu-studio
     bottles
-    freecad-wayland
+    freecad-fixed
     gmsh
     graphviz
     kicad
