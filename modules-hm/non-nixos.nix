ce30c973da75e8900578282b0e6eb91dab1f4e7e
Michael Leuchtenburg <michael@slashhome.org>
2026-02-09 13:15:34 -0500

    non-nixos: use new nixGL option
    The old one is deprecated. In 26.05 I should be able to drop nixGL
    entirely and use the activation script that sets up /run/opengl-drivers
    instead.

--- modules-hm/non-nixos.nix
+++ modules-hm/non-nixos.nix
@@ -7,7 +7,7 @@
 }:
 {
   my.wayland.swaylock_path = "/usr/bin/swaylock";
-  nixGL.packages = inputs.nixGL.packages;
+  targets.genericLinux.nixGL.packages = inputs.nixGL.packages;
   home.packages = with pkgs; [ (config.lib.nixGL.wrap niri) ];
   programs.ghostty.package = with pkgs; (config.lib.nixGL.wrap ghostty);
   programs.alacritty.package = with pkgs; (config.lib.nixGL.wrap alacritty);
