4190d6f575aed0944bdf39fcd0833bffae1ed170
Michael Leuchtenburg <michael@slashhome.org>
2026-03-22 12:40:31 -0400

    nvim: turn up contrast in gruvbox for ui elements
    I'm not sure exactly what these are but they're at least the foldcolumn
    items.

--- home/dotconfig/nvim/lua/config/colors.lua
+++ home/dotconfig/nvim/lua/config/colors.lua
@@ -7,6 +7,7 @@ require("tokyonight").setup {
 
 vim.g.gruvbox_material_background = 'hard'
 vim.g.gruvbox_material_foreground = 'original'
+vim.g.gruvbox_material_ui_contrast = 'high'
 vim.g.gruvbox_material_enable_bold = 1
 vim.g.gruvbox_material_colors_override = { bg0 = { '#171717', '234' } }
 vim.cmd.colorscheme("gruvbox-material")
