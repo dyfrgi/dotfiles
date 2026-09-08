78d786d9702059b167a87ffe6c856dda4b16ac8d
Michael Leuchtenburg <michael@slashhome.org>
2026-09-08 15:08:10 -0400

    nixflix: set up downloaders
    Set up prowlarr to provide both sabnzbd and qbittorrent, with airvpn and
    newshosting API keys. Also configure Sonarr to prefer Usenet.

--- hosts/splat/services/nixflix/default.nix
+++ hosts/splat/services/nixflix/default.nix
@@ -4,14 +4,118 @@
     enable = true;
     mediaDir = "/data/media";
     downloadsDir = "/data/media/downloads";
+    vpn = {
+      enable = true;
+      accessibleFrom = [ "192.168.1.0/24" ];
+      openVPNPorts = [
+        {
+          port = 63633;
+          protocol = "both";
+        }
+      ];
+      wgConfFile = config.age.secrets.airvpn_config_splat.path;
+    };
+    nginx = {
+      enable = true;
+      domain = "uncri.me";
+    };
+
+    # Sonarr - TV
     sonarr = {
       enable = true;
       config = {
         apiKey._secret = config.age.secrets.sonarr_api_key.path;
         hostConfig = {
-          username = "msl";
-          password = "whatever";
+          username._secret = config.age.secrets.arr_username.path;
+          password._secret = config.age.secrets.arr_password.path;
         };
+        delayProfiles = [
+          {
+            enableUsenet = true;
+            enableTorrent = true;
+            preferredProtocol = "usenet";
+            usenetDelay = 0;
+            torrentDelay = 0;
+            bypassIfHighestQuality = true;
+          }
+        ];
+      };
+    };
+
+    # Prowlarr - Indexers
+    prowlarr = {
+      enable = true;
+      config = {
+        apiKey._secret = config.age.secrets.prowlarr_api_key.path;
+        hostConfig.username._secret = config.age.secrets.arr_username.path;
+        hostConfig.password._secret = config.age.secrets.arr_password.path;
+        indexers = [
+          # NZB indexers
+          {
+            enable = true;
+            name = "NZBgeek";
+            apiKey._secret = config.age.secrets.nzbgeek_apikey.path;
+          }
+
+          # Torrent indexers
+          {
+            enable = true;
+            name = "Nyaa.si";
+            baseUrl = "https://nyaa.si/";
+            radarr_compatibility = true;
+            sonarr_compatibility = true;
+          }
+          {
+            enable = true;
+            name = "YTS";
+            baseUrl = "https://yts.bz/";
+          }
+        ];
+      };
+    };
+
+    # Download clients
+    torrentClients.qbittorrent = {
+      enable = true;
+      password._secret = config.age.secrets.arr_password.path;
+      serverConfig = {
+        LegalNotice.Accepted = true;
+        BitTorrent = {
+          Session = {
+            Port = 63633;
+            ReannounceWhenAddressChanged = true;
+          };
+        };
+        Preferences = {
+          WebUI = {
+            Username = "smolwaffle";
+            Password_PBKDF2 = "@ByteArray(iUsNr64A3Eh84pfOEUXr9Q==:5d6csNBNtKCnx7C3cDn45WslQVnQzrjOnGJZgd8tByGV8OeZ710i9Vd4WADxCtYudHXkj4ZCJY3rvTFUNnbXqw==)";
+          };
+        };
+      };
+    };
+    usenetClients.sabnzbd = {
+      enable = true;
+      vpn.enable = false;
+      settings = {
+        misc = {
+          username._secret = config.age.secrets.arr_username.path;
+          password._secret = config.age.secrets.arr_password.path;
+          api_key._secret = config.age.secrets.sabnzbd_api_key.path;
+          nzb_key._secret = config.age.secrets.sabnzbd_nzb_key.path;
+        };
+        servers = [
+          {
+            name = "Newshosting";
+            host = "news.newshosting.com";
+            port = 563;
+            username._secret = config.age.secrets.newshosting_username.path;
+            password._secret = config.age.secrets.newshosting_password.path;
+            connections = 20;
+            ssl = true;
+            priority = 0;
+          }
+        ];
       };
     };
   };
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,31 @@
 {
+  # Generated
   age.secrets.sonarr_api_key = {
     rekeyFile = ./sonarr_api_key.age;
     generator.script = "base64";
   };
+  age.secrets.prowlarr_api_key = {
+    rekeyFile = ./prowlarr_api_key.age;
+    generator.script = "base64";
+  };
+  age.secrets.qbittorrent_password = {
+    rekeyFile = ./qbittorrent_password.age;
+    generator.script = "alnum";
+  };
+  age.secrets.sabnzbd_api_key = {
+    rekeyFile = ./sabnzbd/api_key.age;
+    generator.script = "alnum";
+  };
+  age.secrets.sabnzbd_nzb_key = {
+    rekeyFile = ./sabnzbd/nzb_key.age;
+    generator.script = "alnum";
+  };
+
+  # Manual
+  age.secrets.airvpn_config_splat.rekeyFile = ./airvpn_config_splat.conf.age;
+  age.secrets.newshosting_username.rekeyFile = ./newshosting/username.age;
+  age.secrets.newshosting_password.rekeyFile = ./newshosting/password.age;
+  age.secrets.nzbgeek_apikey.rekeyFile = ./nzbgeek/api_key.age;
+  age.secrets.arr_username.rekeyFile = ./arr/username.age;
+  age.secrets.arr_password.rekeyFile = ./arr/password.age;
 }
